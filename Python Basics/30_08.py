83e9cd9fa22cbe913674b32a514cce130a705a86
faizan102418 <129638545+faizan102418@users.noreply.github.com>
2024-08-30 17:09:04 +0500

    Update 30_08.py

--- Python Basics/30_08.py
+++ Python Basics/30_08.py
@@ -1,10 +1,18 @@
-number = [1,2,3,4,5,6]
+# number = [1,2,3,4,5,6]
 
-def square_num(x:list[int]):
-    square = []
-    for num in x:
-        square.append(num*num) 
-    return square     
+# def square_num(x:list[int]):
+#     square = []
+#     for num in x:
+#         square.append(num*num) 
+#     return square     
         
-print(square_num(number)) 
+# print(square_num(number)) 
   
+  
+numbers = [1,2,3,4,5]
+
+square = map(lambda x : x * x ,numbers)
+print(type(square))  
+
+# squared_num = list(map(lambda x: x * x , numbers))
+# print(squared_num)
