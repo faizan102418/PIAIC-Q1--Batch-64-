ca004cea8625c18819be28399de4e84838ad7efc
faizan102418 <129638545+faizan102418@users.noreply.github.com>
2024-08-30 16:42:03 +0500

    Update 30_08.py

--- Python Basics/30_08.py
+++ Python Basics/30_08.py
@@ -1,1 +1,10 @@
-print("Hello")
+number = [1,2,3,4,5,6]
+
+def square_num(x:list[int]):
+    square = []
+    for num in x:
+        square.append(num*num) 
+    return square     
+        
+print(square_num(number)) 
+  
