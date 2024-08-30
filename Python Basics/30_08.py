881a7f6baf387f9820486febf464c22fb3d9cf28
faizan102418 <129638545+faizan102418@users.noreply.github.com>
2024-08-30 17:52:55 +0500

    Update 30_08.py

--- Python Basics/30_08.py
+++ Python Basics/30_08.py
@@ -11,8 +11,22 @@
   
 numbers = [1,2,3,4,5]
 
-square = map(lambda x : x * x ,numbers)
-print(type(square))  
+square = list(map(lambda x : x * x ,numbers))
+print(square) 
 
 # squared_num = list(map(lambda x: x * x , numbers))
 # print(squared_num)
+
+x = range(6)
+
+# print(x[1],x[2],x[0],x[3],x[4],x[5])
+
+# for n in x:
+#     print(n)
+    
+    
+# for n in range(6):
+#     print(n)    
+
+for x in range(2,6,2):
+    print(x)
